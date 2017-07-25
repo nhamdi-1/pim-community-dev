75bde84b56ca48b39a10c32f7cf44a93ea3b74b8
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2017-07-25 23:35:11 +0200

    API-247: make AttributeValuesResolver implements AttributeValuesResolverInterface to allow overriding

--- src/Pim/Component/Catalog/Manager/AttributeValuesResolver.php
+++ src/Pim/Component/Catalog/Manager/AttributeValuesResolver.php
@@ -15,7 +15,7 @@ use Pim\Component\Catalog\Repository\LocaleRepositoryInterface;
  * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class AttributeValuesResolver
+class AttributeValuesResolver implements AttributeValuesResolverInterface
 {
     /** @var ChannelRepositoryInterface */
     protected $channelRepository;
@@ -51,7 +51,7 @@ class AttributeValuesResolver
      *
      * @return array:array
      */
-    public function resolveEligibleValues(array $attributes, array $channels = null, array $locales = null)
+    public function resolveEligibleValues(array $attributes, array $channels = null, array $locales = null) : array
     {
         $this->channels = $channels;
         $this->locales  = $locales;
